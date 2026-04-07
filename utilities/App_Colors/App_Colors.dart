c3c9caa076755b429a529b695d809e560dfd305c
Pratiksha Thakur <pt9413387@gmail.com>
2026-04-07 13:27:12 +0530

    Add files via upload

--- utilities/App_Colors/App_Colors.dart
+++ utilities/App_Colors/App_Colors.dart
@@ -4,15 +4,23 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color themeColor = Color(0xFF1F78C8);
-  static Color splashBgColor1 = Color(0xFF3AA0F3);
   static Color splashBgColor = Color(0xFFF4F6F8);
   static Color redColor = Color(0xFFFF3B3B);
   static Color opacityBlue = themeColor.withOpacity(0.1);
 
-  static LinearGradient bgGradient = LinearGradient(
+  static LinearGradient darkAvatarGradient = LinearGradient(
+    colors: [
+      AppColors.themeColor,
+      AppColors.themeColor.withOpacity(0.7),
+    ],
+  );
+
+  static LinearGradient lightAvatarGradient =LinearGradient(
+    colors: [
+      Colors.white.withOpacity(0.95),
+      Colors.blue.shade50.withOpacity(0.7),
+    ],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
-    colors: [Color(0xFF4A90E2), Color(0xFFFFB6D5), Color(0xFFE3F2FD)],
-    stops: [0.0, 0.5, 1.0],
   );
 }
---
+++ utilities/App_Strings/app_strings.dart
@@ -0,0 +1,294 @@
+class AppStrings {
+
+  /// POST_CARD_UI ------------------
+  static const String justNow = "Just now";
+  static const String minutesAgo = "min ago";
+  static const String hoursAgo = "hrs ago";
+  static const String daysAgo = "days ago";
+
+  static const String editPost = "Edit Post";
+  static const String deletePost = "Delete Post";
+  static const String reportPost = "Report Post";
+  static const String reportedSuccess = "Post reported successfully";
+
+  static const String resharedPost = "Reshared post";
+  static const String edited = "Edited";
+
+  static const String save = "Save";
+  static const String cancel = "Cancel";
+  static const String delete = "Delete";
+  static const String confirm = "confirm";
+
+  static const String deleteConfirm = "Are you sure?";
+
+  /// PROFILE_SCREEN -----------------
+  static const String completeProfile = "Complete Profile";
+  static const String completeProfileSubtitle = "Complete your profile to continue";
+
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String dateOfBirth = "Date of Birth";
+
+  static const String firstNameRequired = "First name required";
+  static const String lastNameRequired = "Last name required";
+  static const String genderRequired = "Gender required";
+
+  static const String selectDate = "Select date";
+  static const String selectGender = "Select gender";
+
+  static const String male = "Male";
+  static const String female = "Female";
+  static const String other = "Other";
+
+  // Dummy hints
+  static const String hintFirstName = "Pratiksha";
+  static const String hintLastName = "Thakur";
+
+
+  /// PROFILE_EDIT_SCREEN -------------------
+  static const String reAuth ="Re-authenticate";
+  static const String editProfile = "Edit Profile";
+  static const String logout = "Logout";
+  static const String confirmLogout = "Confirm Logout";
+  static const String logoutConfirmMessage = "Are you sure you want to logout?";
+  static const String logoutQ = "Logout?";
+
+  static const String username = "Username";
+  static const String email = "Email";
+  static const String gender = "Gender";
+
+  static const String updateProfile = "Update Profile";
+  static const String update = "Update";
+  static const String updateEmail ="Update email clicked";
+
+  // Validation & Errors
+  static const String error = "Error";
+  static const String emptyUsernameEmail = "Username & Email cannot be empty";
+
+  // Success
+  static const String success = "Success";
+  static const String profileUpdated = "Profile updated successfully";
+
+  // Email verification
+  static const String verifyEmail = "Verify Email";
+  static const String checkEmail = "Check your email to confirm change";
+  static const String verificationSendTo ="VerificationSentTo";
+
+
+
+  static const String verified = "Verified";
+  static const String alreadyVerified = "Your email is already verified";
+
+  static const String emailSent = "Email Sent";
+  static const String verifyEmailMessage = "Check your email to verify your account";
+  static const String emailVerifiedSuccess = "Email verified successfully";
+
+  static const String verify = "Verify";
+
+  static const String failedToSendEmail = "Failed to send email";
+  static const String tooManyRequests = "Too many requests. Try again later.";
+
+  static const String reAuthFailed = "Re-authentication failed";
+  static const String invalidEmail = "Invalid Email";
+  static const String enterValidEmail = "Enter a valid email";
+  static const String emailExists = "Email Exists";
+  static const String emailAlreadyRegistered="This email is already registered";
+
+  static const String defaultAvatar = "U";
+
+  /// forgot password -------------------
+
+  static const String forgotPassword = "Forgot Password";
+  static const String forgotPasswordSubtitle = "Enter your email to reset password";
+
+  static const String emailHint = "example@gmail.com";
+
+  static const String sendResetEmail = "Send Reset Email";
+
+  static const String rememberPassword = "Remember your password?";
+  static const String signIn = "Sign In";
+
+  /// Sign in screen --------------------
+  static const String welcomeBack = "Welcome Back";
+  static const String signInSubtitle = "Sign in to continue";
+
+  static const String password = "Password";
+  static const String passwordHint = "******";
+  static const String enterPassword ="Enter your password";
+
+  static const String forgotPasswordQ = "Forgot Password?";
+
+  static const String or = "or";
+
+  static const String dontHaveAccount = "Don't have an account?";
+  static const String signUp = "Sign Up";
+
+  /// Signup screen ----------
+  static const String createAccount = "Create Account";
+  static const String signupSubtitle = "Join us to get started";
+
+  static const String usernameHint = "username";
+  static const String usernameRequired = "Username is required";
+
+  // Password requirements
+  static const String specialChar = "1 special character";
+  static const String number = "1 number";
+  static const String minLength = "6+ characters";
+  static const String capitalLetter = "1 capital letter";
+
+  // Terms
+  static const String termsAgreement = "I agree to Terms & Privacy Policy";
+
+  // Navigation
+  static const String alreadyHaveAccount = "Already have an account?";
+
+  /// VERIFY_EMAIL -----------------
+  static const String verifyEmailTitle = "Verify Email";
+  static const String verifyEmailSubtitle = "Please verify your email to continue";
+
+  static const String verificationSent =
+  "Verification email sent.\nPlease check your inbox.";
+
+  static const String waitingVerification = "Waiting for email verification...";
+
+  static const String resendEmail = "Resend Email";
+
+  static const String userNotFound = "User not found";
+  static const String emailSentSuccess =
+  "Verification email has been sent successfully";
+
+  static const String somethingWentWrong = "Something went wrong";
+
+  /// CHAT_SCREEN -----------------
+
+  static const String online = "Online";
+  static const String lastSeen = "Last seen";
+
+  static const String selected = "selected";
+
+  static const String typingHint = "Type your message...";
+
+  // Time formats
+  static const String yesterday = "Yesterday";
+
+  // Dialog
+
+  static const String deleteMessage = "Delete Message";
+  static const String deleteMessageConfirm =
+  "Do you want to delete this message ?";
+
+  /// HOME_SCREEN ------------------
+  static const String appName = "pChat";
+
+  static const String noPosts = "No posts yet";
+
+  static const String noInternet = "No Internet";
+  static const String checkInternet = "Please check your internet connection";
+
+  static const String whatsOnMind = "What's on your mind?";
+  static const String create = "Create";
+
+  static const String postDeletedSuccessfully = "Post deleted successfully";
+
+  /// POST_INTERACTION_SHEET --------------
+  // Tabs
+  static const String likes = "Likes";
+  static const String comments = "Comments";
+  static const String deleteComment = "Delete this comment?";
+  static const String reshares = "Reshares";
+
+  // Empty states
+  static const String noLikes = "No likes yet";
+  static const String noComments = "No comments yet";
+  static const String noReshares = "No reshares yet";
+
+  // Actions
+  static const String resharedText = "reshared this post";
+  static const String writeComment = "Write a comment...";
+
+  static const String edit = "Edit";
+  static const String editEmail = "Edit Email";
+  static const String editComment = "Edit Comment";
+
+  ///CREATE_POST_SCREEN----------------
+
+  static const String addPost = "Add Post";
+  static const String post = "Post";
+
+  static const String loading = "Loading...";
+
+
+  ///CREATE_GROUP_SCREEN------------
+  static const String newGroup = "New Group";
+  static const String enterGroupName = "Enter group name";
+
+  static const String members = "Members";
+  static const String addMembers = "Add Members";
+  static const String remove = "Remove";
+
+  static const String createGroup = "Create Group";
+
+  // Validation
+  static const String enterGroupNameError = "Please enter a group name";
+  static const String selectMemberError = "Select at least one member";
+
+  /// PROFILE_SCREEN--------------------
+  static const String friends = "Friends";
+  static const String posts = "Posts";
+
+
+  /// ALL_FRIENDS-----------------------
+  static const String searchFriends = "Search friends";
+  static const String noFriends = "No friends yet";
+
+  static const String failedToRefresh = "Failed to refresh";
+
+  static const String pending = "Pending";
+  static const String accept = "Accept";
+  static const String startChat = "Start Chat";
+  static const String openChat = "Open Chat";
+  static const String openGroup = "Open Group";
+  static const String addFriend = "Add Friend";
+
+  static const String yearsShort = "yrs";
+
+  /// CHAT_FRIENDS------------------
+  // Tabs
+  static const String chats = "Chats";
+  static const String groups = "Groups";
+
+  // Empty states
+  static const String noChats = "No chats yet";
+  static const String noGroups = "No Groups Found";
+
+  // Messages
+  static const String sayHello = "Say hello 👋";
+
+  // Default group avatar
+  static const String defaultGroupAvatar = "G";
+
+  /// friend_REQUEST -------------------
+
+  static const String friendRequests = "Friend Requests";
+
+  // Tabs
+  static const String received = "Received";
+  static const String sent = "Sent";
+
+  // Messages
+  static const String requestReceived = "Sent you a friend request";
+  static const String requestPending = "Request pending...";
+
+  // Actions
+  static const String reject = "Reject";
+
+  // Default fallback
+  static const String defaultUser = "User";
+
+  /// SEARCH_SCREEN
+  static const String searchUsers = "Search users";
+  static const String noUser = "No User Found";
+
+
+
+}
